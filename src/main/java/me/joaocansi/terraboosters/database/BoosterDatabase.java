82b9aeed3c6d531d52318a9654acda9990e1402d
BuildTools <unconfigured@null.spigotmc.org>
2024-01-03 13:35:11 -0300

    fix: non-expired boosters deleted when disconnect()

--- src/main/java/me/joaocansi/terraboosters/database/BoosterDatabase.java
+++ src/main/java/me/joaocansi/terraboosters/database/BoosterDatabase.java
@@ -2,6 +2,7 @@ package me.joaocansi.terraboosters.database;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.db.DatabaseType;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.stmt.DeleteBuilder;
 import com.j256.ormlite.stmt.Where;
@@ -10,7 +11,9 @@ import com.j256.ormlite.table.TableUtils;
 import me.joaocansi.terraboosters.Main;
 import me.joaocansi.terraboosters.entities.Booster;
 import me.joaocansi.terraboosters.utils.console.Console;
+import org.bukkit.configuration.file.FileConfiguration;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.Date;
 import java.util.List;
@@ -20,10 +23,7 @@ public final class BoosterDatabase {
 
     public BoosterDatabase() {
         try {
-            String url = "jdbc:sqlite:" + Main.getPlugin().getDataFolder() + "/storage.db";
-            ConnectionSource connection = new JdbcConnectionSource(url);
-            TableUtils.createTableIfNotExists(connection, Booster.class);
-            dao = DaoManager.createDao(connection, Booster.class);
+            connect();
         } catch (SQLException e) {
             Console.error("Not able to connect to the database. Here's the error: \n" + e.getMessage());
         }
@@ -56,13 +56,36 @@ public final class BoosterDatabase {
         }
     }
 
+    public void connect() throws SQLException {
+        FileConfiguration config = Main.getPlugin().getConfig();
+        String type = config.getString("settings.storage.type");
+        ConnectionSource connection;
+
+        if (type == null || !type.equalsIgnoreCase("MySQL")) {
+            String url = "jdbc:sqlite:" + Main.getPlugin().getDataFolder() + "/storage.db";
+            connection = new JdbcConnectionSource(url);
+        } else {
+            String username = config.getString("settings.storage.username");
+            String password = config.getString("settings.storage.password");
+            String database = config.getString("settings.storage.database");
+            String host = config.getString("settings.storage.host");
+            String port = config.getString("settings.storage.port");
+
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+            connection = new JdbcConnectionSource(url, username, password);
+        }
+
+        TableUtils.createTableIfNotExists(connection, Booster.class);
+        dao = DaoManager.createDao(connection, Booster.class);
+    }
+
     public void disconnect() {
         Date date = new Date();
         try {
             DeleteBuilder<Booster, Integer> deleteBuilder = dao.deleteBuilder();
             deleteBuilder
                     .where()
-                    .gt("expiresIn", date.getTime());
+                    .lt("expiresIn", date.getTime());
             dao.delete(deleteBuilder.prepare());
         } catch (SQLException e) {
             Console.error("Not able to delete boosters in database. Here's the error: \n" + e.getMessage());
